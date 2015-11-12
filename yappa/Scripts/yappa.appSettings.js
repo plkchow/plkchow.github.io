a171a900464320ff7d2fc9f866d90fe5f4152870
plkchow <plkchow@gmail.com>
2015-11-12 13:41:09 -0800

    removed redundant api endpoints

--- yappa/Scripts/yappa.appSettings.js
+++ yappa/Scripts/yappa.appSettings.js
@@ -2,9 +2,7 @@ var yappa;
 (function (yappa) {
     var AppSettings = (function () {
         function AppSettings() {
-            //baseUrl = "https://microsoft-apiappbafd7abfae1e4958a624350cc194ab2d.azurewebsites.net/api";
-            //baseUrl = "http://localhost:19662/api";
-            this.baseUrl = "http://vchehealthws07.vch.ca/yappa.api/api";
+            this.baseUrl = "https://microsoft-apiappbafd7abfae1e4958a624350cc194ab2d.azurewebsites.net/api";
         }
         return AppSettings;
     })();
